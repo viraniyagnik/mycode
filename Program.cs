b98fbc02120cf86b01aae3ecc37a2ffc9d14c333
viraniyagnik <77527826+viraniyagnik@users.noreply.github.com>
2021-01-29 12:05:10 +0530

    code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String F_name, L_name, B_date;
+            Console.Write("Enter first name : ");
+            F_name = Console.ReadLine();
+            Console.Write("Enter last name : ");
+            L_name = Console.ReadLine();
+            Console.Write("Enter date of birth : ");
+            B_date = Console.ReadLine();
+            Console.WriteLine(F_name+ " , " +L_name+ " , " +B_date);
+        }
+    }
+}
